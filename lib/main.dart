dfab9023bc27cc50b321a07592a4def8bcf35722
Kweayon  Clark <Kweayon@gmail.com>
2022-09-07 14:02:24 -0500

    Updated
    Created base layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,39 @@
+// A Simple Calculator App
 
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key); // initalier list
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false, // Faster compile time??
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Simple Calculator',
+          style: TextStyle(), //TODO: Titls text styling
+        ),
+      ),
+    );
+  }
+}
