21f32b7483a3f04ccbefeaceb0ef989b3a7b8b39
Kweayon  Clark <Kweayon@gmail.com>
2022-09-07 20:03:41 -0500

    Update
    Refactoring

---
+++ lib/src/Models/button_values.dart
@@ -0,0 +1,25 @@
+class ButtonValue {
+  final List<String> _buttons = [
+    'C',
+    '+/-',
+    '%',
+    'DEL',
+    '7',
+    '8',
+    '9',
+    '/',
+    '4',
+    '5',
+    '6',
+    'x',
+    '1',
+    '2',
+    '3',
+    '-',
+    '0',
+    '.',
+    '=',
+    '+',
+  ];
+  List<String> get buttons => _buttons;
+}
