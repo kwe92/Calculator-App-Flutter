e24d2027768f1f30e5e83f8d00ad62c54c6ea0a3
Kweayon  Clark <Kweayon@gmail.com>
2022-10-24 19:09:54 -0500

    Update
    Refactored butons to be neumorphic

--- lib/src/common_widgets/buttons.dart
+++ lib/src/common_widgets/buttons.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_neumorphic/flutter_neumorphic.dart';
+
+import '../constants/layout.dart';
 
 class MyButton extends StatelessWidget {
   const MyButton(
@@ -18,23 +21,51 @@ class MyButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return FloatingActionButton(
-        shape: Border.all(
-          color: Colors.black26,
-        ),
-        elevation: 0,
+    return ConstrainedBox(
+      constraints: BoxConstraints(
+          minHeight: 10, maxHeight: 10, minWidth: 10, maxWidth: 10),
+      child: NeumorphicButton(
         onPressed: buttontapped,
-        child: Container(
+        style: NeumorphicStyle(
+          shape: NeumorphicShape.concave,
+          boxShape: NeumorphicBoxShape.roundRect(BorderRadius.circular(100)),
+          depth: 2,
+          lightSource: LightSource.topLeft,
+          shadowLightColor: const Color.fromRGBO(235, 234, 234, 1),
+          shadowDarkColor: const Color.fromRGBO(190, 190, 190, 1),
           color: color,
-          child: Center(
-            child: Text(
-              buttonText,
-              style: TextStyle(
-                  fontSize: fontSize,
-                  color: textColor,
-                  fontWeight: FontWeight.w600),
-            ),
+          //const Color.fromRGBO(53, 53, 53, 1)
+        ),
+        child: Center(
+          child: Text(
+            buttonText,
+            style: TextStyle(
+                fontSize: fontSize,
+                color: textColor,
+                fontWeight: FontWeight.w600),
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
+
+
+// FloatingActionButton(
+//         shape: Border.all(
+//           color: Colors.black26,
+//         ),
+//         elevation: 0,
+//         onPressed: buttontapped,
+//         child: Container(
+//           color: color,
+//           child: Center(
+//             child: Text(
+//               buttonText,
+//               style: TextStyle(
+//                   fontSize: fontSize,
+//                   color: textColor,
+//                   fontWeight: FontWeight.w600),
+//             ),
+//           ),
+//         ));
