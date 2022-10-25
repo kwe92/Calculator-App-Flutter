5f5654233744dd28c3696937608eaf8cae3e5dc1
Kweayon  Clark <Kweayon@gmail.com>
2022-10-24 19:12:05 -0500

    Update

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,14 +1,33 @@
 import 'package:calculatorapp/src/features/calculator_page/calculator_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_neumorphic/flutter_neumorphic.dart';
+
+import 'constants/layout.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key); // initalier list
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false, // Faster compile time??
+    return const NeumorphicApp(
+      // debugShowCheckedModeBanner: false,
+      // title: 'Flutter Demo',
+      // themeMode: ThemeMode.dark,
+      // theme: NeumorphicThemeData(
+      //   baseColor: Color(0xFFFFFFFF),
+      //   lightSource: LightSource.topLeft,
+      //   depth: 10,
+      // ),
+      // darkTheme: NeumorphicThemeData(
+      //   baseColor: AppColor.main,
+      //   lightSource: LightSource.topLeft,
+      //   depth: 6,
+      // ),
       home: CalculatorScreen(),
     );
+    // return const MaterialApp(
+    //   debugShowCheckedModeBanner: false, // Faster compile time??
+    //   home: CalculatorScreen(),
+    // );
   }
 }
