db603e4a673cb96bf8f890caad524502e62722ad
Kweayon  Clark <Kweayon@gmail.com>
2022-10-28 10:52:41 -0500

    Old Calculator Screen

---
+++ lib/src/old_version/old_neumorphic_window.dart
@@ -0,0 +1,39 @@
+            // Expanded(
+            //   child: Padding(
+            //     padding: const EdgeInsets.all(10.0),
+            //     child: Neumorphic(
+            //       style: neuStyle,
+            //       child: Column(
+            //         children: <Widget>[
+            //           Container(
+            //             padding: const EdgeInsets.all(Sizes.p20),
+            //             alignment: Alignment.centerRight,
+            //             child: Text(
+            //               userInputString,
+            //               style: const TextStyle(
+            //                   fontSize: 22,
+            //                   color: Color.fromARGB(
+            //                       255, 87, 44, 44)) //TODO: Text Style
+            //               ,
+            //             ),
+            //           ),
+            //           Expanded(
+            //             child: Container(
+            //               alignment: Alignment.bottomRight,
+            //               padding: const EdgeInsets.fromLTRB(
+            //                   Sizes.p12, 0, Sizes.p12, Sizes.p12),
+            //               child: Text(
+            //                 //answer,
+            //                 userInput,
+            //                 style: const TextStyle(
+            //                     fontSize: Sizes.p32,
+            //                     color: AppColor
+            //                         .textMain), // may need to be smaller
+            //               ),
+            //             ),
+            //           )
+            //         ],
+            //       ),
+            //     ),
+            //   ),
+            // ),
